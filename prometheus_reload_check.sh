292387abbeb551369d77e05617908c153b7e8507
zbbkeepgoing <42572980+zbbkeepgoing@users.noreply.github.com>
2019-05-24 13:48:37 +0800

    Update prometheus_reload_check.sh

--- prometheus_reload_check.sh
+++ prometheus_reload_check.sh
@@ -1,19 +1,20 @@
 file_path=/etc/prometheus/prometheus.yml
 file_old_stat="`stat ${file_path}|grep Modify`"
 while [[ true ]]; do
-  sleep 2s
+  sleep 5s
+  now=$(date +%c)
   if [ -f "$file_path" ]; then
      file_new_stat="`stat ${file_path}|grep Modify`"
      if [[ `echo ${file_old_stat}` == `echo ${file_new_stat}` ]]; then
-        echo "### In 5s ,${file_path} does not change ###"
+        echo "${now} : In 5s ,${file_path} does not change"
      else
-        echo ${file_new_stat}
-        echo ${file_old_stat}
+        echo "${now} : file change"
+        echo "${now} : new file --> ${file_new_stat}"
+        echo "${now} : new file --> ${file_old_stat}"
         file_old_stat=${file_new_stat}
-        echo "file change"
         curl -XPOST http://127.0.0.1:9090/-/reload
      fi
   else
-    echo "file no exit "
+    echo "${now} : file no exit "
   fi
 done
