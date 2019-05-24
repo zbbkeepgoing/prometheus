87199ff8c09fef1d12a09dcb4ec32d4304559bdb
zbbkeepgoing <42572980+zbbkeepgoing@users.noreply.github.com>
2019-05-24 16:46:42 +0800

    Update prometheus_start.sh

--- prometheus_start.sh
+++ prometheus_start.sh
@@ -1,6 +1,6 @@
 # prometheus reload check 
 # if /etc/prometheus/prometheus.yaml modify, this sh will curl the reload request of prometheus.
-nohup ./prometheus_reload_check.sh	> /tmp/reload_check.log 2>&1
+nohup ./prometheus_reload_check.sh	> /tmp/reload_check.log 2>&1 &
 
 # --web.enable-lifecycle
 # enable reload prometheus.yaml of prometheus 
