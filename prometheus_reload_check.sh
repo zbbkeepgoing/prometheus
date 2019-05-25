dc308dbbeeb3815683454aa118465d58c2bf0e1a
zbbkeepgoing <42572980+zbbkeepgoing@users.noreply.github.com>
2019-05-25 13:13:25 +0800

    Update prometheus_reload_check.sh

--- prometheus_reload_check.sh
+++ prometheus_reload_check.sh
@@ -5,7 +5,7 @@ while [[ true ]]; do
   now=$(date +%c)
   if [ -f "$file_path" ]; then
      file_new_stat="`stat ${file_path}|grep Modify`"
-     if [[ `echo ${file_old_stat}` == `echo ${file_new_stat}` ]]; then
+     if [ "$file_old_stat" == "$file_new_stat" ]; then
         echo "${now} : In 5s ,${file_path} does not change"
      else
         echo "${now} : file change"
